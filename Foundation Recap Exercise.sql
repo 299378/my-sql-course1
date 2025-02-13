0b30c34ee14ade51ff4d2cd1273e3056fc0a0071
299378 <mahbuba.uddin@nhs.net>
2025-02-13 16:39:54 +0000

    Questions

--- Foundation Recap Exercise.sql
+++ Foundation Recap Exercise.sql
@@ -8,7 +8,8 @@ This lists 44 patients admitted to London hospitals over 5 days between Feb 26th
 SELECT
 	*
 FROM
-	PatientStay ps ;
+	PatientStay ps
+;
 
 /*
 1. List the patients -
@@ -23,15 +24,33 @@ c) only the Surgery wards
 
 -- Write the SQL statement here
 SELECT
-    ps.PatientId, ps.AdmittedDate, ps.DischargeDate, ps.Ward, ps.Hospital
-    , DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) + 1 AS LengthOfStay
+	ps.PatientId
+	,ps.AdmittedDate
+	,ps.DischargeDate
+	,ps.Ward
+	,ps.Hospital
+	,DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) + 1 AS LengthOfStay
 FROM
-    PatientStay ps
-WHERE ps.Hospital in ('Oxleas', 'PRUH')
-AND MONTH(ps.AdmittedDate) = 2
-AND ps.AdmittedDate BETWEEN '2024-02-01' AND '2024-02-29'
-and PS.Ward like '%Surgery'
+	PatientStay ps
+WHERE ps.Hospital IN ('Oxleas', 'PRUH')
+	--AND MONTH(ps.AdmittedDate) = 2
+	AND ps.AdmittedDate BETWEEN '2024-02-01' AND '2024-02-29'
+	--AND ps.AdmittedDate >= '2024-02-01'
+	--AND ps.AdmittedDate <= '2024-02-29'
+	AND PS.Ward LIKE '%Surgery'
 ORDER BY ps.AdmittedDate DESC, ps.PatientId DESC
+
+
+--test date functions 
+SELECT
+	month ('2024-02-13') AS MyMonth
+SELECT
+	DATEPART(MM, 2024-02-13)
+SELECT
+	DATENAME(MM, '2024-02-13')
+
+SELECT
+	LEFT(DATENAME(MM, '2024-02-13'),3) AS Month
 
 /*
 5. How many patients has each hospital admitted? 
@@ -41,4 +60,78 @@ ORDER BY ps.AdmittedDate DESC, ps.PatientId DESC
 */
 
 -- Write the SQL statement here
+
+SELECT
+	ps.hospital
+	,count (*) AS [NumberOfPatients]
+	,SUM(ps.Tariff) AS TotalTariff
+FROM
+	PatientStay ps
+GROUP BY ps.Hospital
+HAVING SUM(ps.Tariff) >50
+ORDER BY SUM(ps.tariff) DESC
+
+SELECT* from DimHospitalBad h
+
+SELECT
+	ps.PatientId
+    ,ps.AdmittedDate
+	,h.Hospital
+	,ps.Hospital
+	,h.Type
+	,ps.Ethnicity
+FROM
+	PatientStay ps FULL OUTER JOIN DimHospitalBad h ON ps.Hospital=h.Hospital
+	WHERE ps.Ethnicity is NULL
+
+
+	SELECT
+    ps.PatientId
+    ,ps.AdmittedDate
+    ,ps.DischargeDate
+    ,ps.Ward
+    ,ps.Hospital
+    ,DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) + 1 AS LengthOfStay
+SELECT
+    CASE
+        WHEN ps.Tariff >= 7 THEN 'High Cost'
+        WHEN ps.Tariff >= 4 THEN 'Medium Cost'
+        ELSE 'Low Cost' END AS CostType
+		,COUNT(*) as [Number of Patients]
+		,SUM(ps.Tariff) as [Total cost]
+FROM
+    PatientStay ps
+	WHERE CASE
+        WHEN ps.Tariff >= 7 THEN 'High Cost'
+        WHEN ps.Tariff >= 4 THEN 'Medium Cost'
+        ELSE 'Low Cost' END IN ('High cost', 'Low cost')
+GROUP BY CASE
+        WHEN ps.Tariff >= 7 THEN 'High Cost'
+        WHEN ps.Tariff >= 4 THEN 'Medium Cost'
+        ELSE 'Low Cost' END
+;
+
+--adding cte- grouping to make it simpler
+WITH
+	cte
+	AS
+	(
+		SELECT
+			CASE
+        WHEN ps.Tariff >= 7 THEN 'High Cost'
+        WHEN ps.Tariff >= 4 THEN 'Medium Cost'
+        ELSE 'Low Cost' END AS CostType
+		,ps.Tariff
+		FROM
+			PatientStay ps
+	)
+SELECT
+	cte.CostType
+	,count(*) AS NumPatients
+	,SUM(cte.Tariff) AS TotalTariff
+FROM
+	cte
+WHERE cte.CostType IN ('High Cost', 'Low Cost')
+GROUP BY cte.CostType
+
 
